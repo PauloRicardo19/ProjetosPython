b580360dd1223f963359dafd440af93e84139295
PauloRicardo19 <joilsonigino4@gmail.com>
2026-09-24 19:47:06 -0300

    Refactor velocity calculation and output
    Refactor Velocidade_Média to return velocity instead of printing. Initialize variables in main and print velocity after calculation.

--- Modularização/modu27.py
+++ Modularização/modu27.py
@@ -1,8 +1,5 @@
 #calcular e mostrar velocidade media em km/h com passagem de parametros
 
-voltas: int = 0
-extensão: float = 0.0
-tempo: float = 0.0
 
 def Velocidade_Média(v ,e ,t ):
     distancia: float = 0.0
@@ -12,16 +9,19 @@ def Velocidade_Média(v ,e ,t ):
     distancia = distancia / 1000
     Horas = t / 60
     velo = distancia / Horas
-    print (velo,"Km/h")
+    return velo
 
 def main():
-    global voltas
-    global extensão
-    global tempo
+    voltas: int = 0
+    extensão: float = 0.0
+    tempo: float = 0.0
+    velo = Velocidade_Média(voltas,extensão, tempo)
     voltas = int(input("Insira a quantidade de voltas: "))
     extensão = float(input("Insira a extensão da pista: "))
     tempo = float(input("Insira a duração em MINUTOS: "))
     Velocidade_Média(voltas, extensão, tempo)
+    print (velo,'km/h') 
+    
 
 if __name__ == '__main__':
     main()
