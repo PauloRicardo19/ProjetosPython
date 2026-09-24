107863a063fdcd90b1ede9653abc7fd0f51c8109
PauloRicardo19 <joilsonigino4@gmail.com>
2026-09-24 19:48:18 -0300

    Refactor investment calculation to return values
    Refactor Tipo_Investimento function to return values instead of printing. Update main function to print the returned value.

--- Modularização/modu29.py
+++ Modularização/modu29.py
@@ -1,25 +1,28 @@
 #receber tipo de investimento com passagem de parâmetro
 
-investimento: int = 0
-valor: float = 0.0
+
 
 def Tipo_Investimento(I, V):
     NovoValor: float = 0.0
     if I == 1:
         NovoValor = V + (V * 0.03)
-        print ("Em 30 dias a renda vai ser:",NovoValor)
+        return f"Em 30 dias a renda vai ser: {NovoValor}"
     elif I == 2:
         NovoValor = V + (V * 0.05)
-        print ("Em 30 dias a renda vai ser:",NovoValor)
+        return f"Em 30 dias a renda vai ser: {NovoValor}"
     else:
-        print('Insira um tipo de investimento válido.')
-
+        return 'Insira um tipo de investimento válido.'
+    
 def main():
-    global investimento
-    global valor
+    investimento: int = 0
+    valor: float = 0.0
+    NV: float = 0.0
     investimento = int(input("Qual o tipo de investimento? "))
     valor = float(input("Qual o valor do depósito? "))
-    Tipo_Investimento(investimento, valor)
+    NV = Tipo_Investimento(investimento, valor)
+    print (NV)
+
+
 
 if __name__ == '__main__':
     main()
